7710a12bfa823b1877e84347f1740d6dc0e1c2b1
eric <phantomenace87@gmail.com>
2023-10-30 12:43:35 -0700

    Made bag work

--- CSC220/src/GUI/Bag.java
+++ CSC220/src/GUI/Bag.java
@@ -5,36 +5,46 @@ import java.util.Collections;
 import java.util.List;
 
 public class Bag {
-    private static List<Character> bag = new ArrayList<Character>(7);
-
-    public static void shuffle(){
-        Collections.shuffle(bag);
+    private static List<Character> refillBag = new ArrayList<Character>(7);
+    private static List<Character> totalBag = new ArrayList<Character>(11);
+    
+    static {
+        refillBag.add('T');
+        refillBag.add('L');
+        refillBag.add('J');
+        refillBag.add('O');
+        refillBag.add('S');
+        refillBag.add('Z');
+        refillBag.add('I');
     }
 
-    public static Character getNext() {
-        if (bagIsEmpty()){
-            refillBag();
+    private static final int MINIMUM_SIZE = 4;
+    
+    public static void refillAndShuffle(){
+        Collections.shuffle(refillBag);
+        totalBag.addAll(refillBag);
+    }
+
+    public static Character getNextPiece() {
+        if (totalBag.size() <= MINIMUM_SIZE){
+            refillAndShuffle();
         }
 
-        return bag.remove(0);
+        return totalBag.remove(0);
+    }
+
+    public static Character[] getNext(){
+        Character[] arr = new Character[MINIMUM_SIZE];
+
+        for (int i = 0; i < MINIMUM_SIZE; i++){
+            arr[i] = totalBag.get(i);
+        }
+
+        return arr;
     }
     
 
-    private static boolean bagIsEmpty() {
-        return bag.isEmpty();
-    }
-    
-    private static void refillBag() {
-        bag.add('T');
-        bag.add('L');
-        bag.add('J');
-        bag.add('O');
-        bag.add('S');
-        bag.add('Z');
-        bag.add('I');
-
-        shuffle();
-    }
+    // TODO: KEEP TRACK OF BAG, REFILL BAG, TOTAL BAG
 
     private Bag(){
         // shouldn't instantiate Bag
